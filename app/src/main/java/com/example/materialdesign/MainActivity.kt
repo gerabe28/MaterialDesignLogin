96deea5b576cb742d0e8a5c5495f2348538c2b3d
gerabe28 <58001576+gerabe28@users.noreply.github.com>
2020-01-06 12:20:28 -0500

    Splash

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.materialdesign
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button.setOnClickListener {
-
-            val intent = Intent(this,Tryinh::class.java)
-            startActivity(intent)
-        }
-
+        val SPLASH_TIME_OUT = 2000L
+        val homeIntent = Intent(this@MainActivity, Tryinh::class.java)
+        Handler().postDelayed({
+            //Do some stuff here, like implement deep linking
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
