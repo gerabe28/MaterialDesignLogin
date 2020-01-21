7c269230b99dfd89530dc70ca3cb96efd1e04e7d
juan <jcerron@creditsesame.com>
2020-01-21 10:56:43 -0500

    Update

--- app/src/main/java/com/example/materialdesign/HomeActivity.kt
+++ app/src/main/java/com/example/materialdesign/HomeActivity.kt
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
 package com.example.materialdesign
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.nav_header.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -19,19 +16,17 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         toolbar("App")
 
-       
-        
 
         //recepcion de objeto USER
-      intent.extras.getString("correo")
-       // intent.getShortExtra("correo","user")
+        intent.extras.getString("correo")
+        // intent.getShortExtra("correo","user")
         val intent = getIntent();
         val emailReceived = intent.getStringExtra("correo")
 
         Log.d("tag","Email: "+emailReceived)
 
-        
-        navigation.setNavigationItemSelectedListener ({item ->
+
+        navigation.setNavigationItemSelectedListener { item ->
             when(item.itemId){
                 R.id.op1 -> {
                     Log.i("OP1","Opcion 1")
@@ -45,13 +40,13 @@ class HomeActivity : AppCompatActivity() {
                     Log.i("OP3","Cerrar Sesión")
 
                     val user = FirebaseAuth.getInstance().signOut()
-                    val intent = Intent(this,LoginActivity::class.java)
+                    val intent = Intent(this, LoginActivity::class.java)
                     startActivity(intent)
 
                 }
             }
             false
-        })
+        }
     }
 
 
@@ -78,88 +73,3 @@ class HomeActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 }
-
-// https://www.youtube.com/watch?v=0-Xq2ti_HaE
-=======
-package com.example.materialdesign
-
-import android.os.Bundle
-import android.view.KeyEvent
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
-
-import com.example.arch.BaseActivity
-import com.example.arch.navigator.Navigator
-import kotlinx.android.synthetic.main.activity_home.*
-
-
-//@Viewable(presenter = HomeActivityPresenter.class, layout = R.layout.activity_home)
-class HomeActivity : BaseActivity<HomeActivityContract.View, HomeActivityContract.Presenter>(),
-    HomeActivityContract.View, View.OnClickListener {
-
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        showToolbarUp("tittle", true)
-
-        action.setOnClickListener {
-            presenter?.navigationHome()
-        }
-
-    }
-
-    override fun openDetailFragment() {
-        /*Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(this);*/
-        /*if (getSupportFragmentManager().findFragmentById(R.id.pageLayout) == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.pageLayout, new HomeFirstFragment())
-                    .commit();
-        }*/
-    }
-
-    override fun replaceFragment(n: Navigator) {
-
-    }
-
-    fun showLoading() {
-
-    }
-
-    fun hideLoading() {
-
-    }
-
-    fun showError(errorMessage: String) {
-
-    }
-
-    override fun onBack() {
-        Toast.makeText(this,"Hola",Toast.LENGTH_LONG).show()
-    }
-
-
-    override fun onClick(view: View) {
-        presenter!!.navigationHome()
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-
-        }
-        return super.onKeyDown(keyCode, event)
-    }
-
-    override fun initPresenter(): HomeActivityContract.Presenter {
-        return HomeActivityPresenter()
-    }
-
-    fun showToolbarUp(tittle: String, upButton: Boolean) {
-        //super.showToolbarUp(tittle, upButton);
-    }
-}
->>>>>>> 2b0c12480bc77ddd64f3a668654e240ccf9a68de
